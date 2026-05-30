39e79d612a155d33efdace3f01538efdbdc747ce
Brown-cmd <browngervasorie@gmail.com>
2026-05-31 01:10:53 +0300

    backend and frontend setup via spring boot and react

---
+++ BackEnd/src/main/java/Brown/TeleMetryStream/TeleMetryStreamApplication.java
@@ -0,0 +1,13 @@
+package Brown.TeleMetryStream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeleMetryStreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeleMetryStreamApplication.class, args);
+	}
+
+}
---
+++ BackEnd/src/test/java/Brown/TeleMetryStream/TeleMetryStreamApplicationTests.java
@@ -0,0 +1,13 @@
+package Brown.TeleMetryStream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeleMetryStreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
